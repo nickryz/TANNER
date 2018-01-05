293d43fee4d0194c21c651c0102552bd64f85625
nickryz <nickryz87@gmail.com>
2018-01-05 17:19:10 +0200

    first version

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+
+window.addEventListener('DOMContentLoaded', init);
+
+// import $ from 'jquery';
+
+function init () {
+    var ids = document.querySelector('.bloc');
+    console.log('asdfsadf')
+    window.addEventListener('click', function(e){
+        console.log('asdfsadf')
+        new Vivus('svg', { type: 'delayed', duration: 200 }); 
+
+    })
+   
+}
